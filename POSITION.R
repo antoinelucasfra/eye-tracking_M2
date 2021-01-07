00ec8317844e5a84b1285fd32f28efd460690024
Flavie <flavie.thevenard@gmail.com>
2021-01-07 12:15:49 +0100

    pas besoin

--- POSITION.R
+++
@@ -1,3 +0,0 @@
-library("car")
-
-square_pos = read.csv("25 square position.csv", sep =";", header = TRUE, row.names = 1, dec = ".", colClasses = c("col" = "factor", "xvec" = "numeric", 'yvec' = "numeric"))
