723639267ba07de11a3b0d7df96c694b597dcc89
Julien PETOT <julien.petot@yahoo.fr>
2021-01-15 18:05:50 +0100

    import google drive data(user_time_exp)

--- script/2_heatmap/main_heatmap.R
+++ script/2_heatmap/main_heatmap.R
@@ -1,9 +1,10 @@
 # script to execute the heatmap generation 
 
+library(gsheet)
 
 ########################### parameters #######################
 
-consumers_name = "3-juju"
+consumers_name = "4-thomas"
 consumers_number = 1
 screen_size_input = c(9,16)
 
@@ -12,6 +13,22 @@ start_time = rep(5,17)
 end_time = rep(60,17)
 
 ##############################################################
+
+# download time_user_exp from google drive 
+
+drive_path = "Eyetracking/1ter_resultats_exp/time_user_exp"
+folder_path = "data/time_user_exp.csv"
+
+drive_download(drive_path, path = folder_path, overwrite = TRUE)
+
+time_user_exp  = read.csv(folder_path, sep = ",", header = TRUE)
+
+
+start_time_vec = time_user_exp[,grepl( "start_time", col, fixed = TRUE)]
+end_time_vec = time_user_exp[,grepl( "end_time", col, fixed = TRUE)]
+  
+# script to download all the screen recorder path from google drive 
+source("script/1_data_processing/download_googleDrive_data.R")
 
 # script to obtain the gaze data on the stimuli (16)
 source("script/1_data_processing/main_data_process.R")
@@ -32,7 +49,7 @@ consumers_list <- df_random[consumers_number,]
 
 # get the list_img in the order for a given consumer with their full path 
 list_img_order <- paste0("experience/cockpit_utile/",
-                         list_img[order(match(substr(list_img,start=1,stop = 1),
+                         list_img[order(match(substr(list_img,start=1,stop = 3),
                                               consumers_list))])
 
 # execute heatmap function 
@@ -43,7 +60,7 @@ for(i in 2:length(stimu_lvl)){
   df_corrected <- df[,1:4]
   colnames(df_corrected) <- col
   heatmap_generator(df_corrected[as.character(df_corrected$stimu) == stimu_lvl[i],],
-                    path_img = list_img_order[i], 
+                    path_img = list_img_order[i-1], 
                     width_size = 640, height_size = 360, transparency_img = 0.6)
 }
 
