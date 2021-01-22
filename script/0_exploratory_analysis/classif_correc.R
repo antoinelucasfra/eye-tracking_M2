7227c9d31b5cb4ff20f3745680dcd8d70a6d1641
Antoine LUCAS <antoine.lucas.fra@gmail.com>
2021-01-22 16:33:43 +0100

    add all data - 1st part

--- script/0_exploratory_analysis/classif_correc.R
+++ script/0_exploratory_analysis/classif_correc.R
@@ -9,6 +9,10 @@ square_pos = read.csv("experience/25_square_position.csv",
                       colClasses = c("col" = "factor", "xvec" = "numeric", 'yvec' = "numeric"))
 square_pos$name = rownames(square_pos)
 
+correspondance = read.csv("experience/correspondance_stimu_couleur.csv", sep =";")
+
+square_correspondance = left_join(square_pos,correspondance,by="col")
+
 
 ################################################
 # define area number we were looking on screen for calibration phase 
@@ -39,6 +43,18 @@ for (i in 1:n_stimuli){
   df = rbind(df,df_temp)
 }
 
+# rename df avec les noms des stimulis A FAIRE DANS MAIN 
+
+plan_exp <- read.csv("experience/plan_exp.csv")[,-1]
+consumers_list <- plan_exp[consumers_number,]
+
+
+levels(df$stimu) <- c("0_correction",as.numeric(consumers_list))
+consumers_list
+
+time_user_exp
+
+
 df_result1 <- df %>% filter(stimu %in% "Result1")
 
 
@@ -59,6 +75,16 @@ df_calib <- df_result1_rm %>% filter(t < 10 + start_time_vec[1,2])
 
 df_result1_rm_nocalib <- df_result1_rm %>% filter(t > 10 + start_time_vec[1,2] )
 
+
+
+
+
+ggplot() +
+  geom_point(df_result1_rm_nocalib,aes(x,y)) +
+  geom_point(square_correspondance %>% filter(stimu=))
+
+
+
 ggplot(df_calib, aes(x = x, y = y) ) +
   
   # theme_void() +
@@ -72,6 +98,34 @@ ggplot(df_calib, aes(x = x, y = y) ) +
                   alpha = 0.8) +
   
   scale_fill_distiller(palette= "Spectral", direction=-1) 
+  scale_x_continuous(limits = c(0, 16)) +
+  scale_y_continuous(limits = c(0, 9)) +
+  
+  # coord_fixed(ratio = 1, xlim = c(0,16), ylim = c(0,9))+
+  
+  theme(
+    legend.position='none', 
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank()
+  )
+
+ggplot(df_result1_rm_nocalib, aes(x = x, y = y) ) +
+    
+    # theme_void() +
+    
+    stat_density_2d(aes(fill = ..density..), 
+                    geom = "raster", 
+                    contour = F, 
+                    contour_var = "ndensity",
+                    n = 200, # use to adjust density estimation and grid quality 
+                    # h = c(5,5),
+                    alpha = 0.8) +
+    
+    scale_fill_distiller(palette= "Spectral", direction=-1) 
   # scale_x_continuous(limits = c(0, 16)) +
   # scale_y_continuous(limits = c(0, 9)) +
   
@@ -86,8 +140,6 @@ ggplot(df_calib, aes(x = x, y = y) ) +
     axis.text.y = element_blank(),
     axis.ticks.y = element_blank()
   )
-
-
 
 
 
