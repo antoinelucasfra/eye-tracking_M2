cf87215e5305c9f42f37fb953a95c4d409219632
Antoine LUCAS <antoine.lucas.fra@gmail.com>
2021-01-26 15:01:34 +0100

    add the cnn_lime sccript

--- script/3_ML_interpretability/cnn_lime.R
+++ script/3_ML_interpretability/cnn_lime.R
@@ -43,7 +43,6 @@ width_size = 640
 # execute function
 cnn_input <- loader_heatmap(method_name = method_name, consumers_data = time_user)
 
-
 #### create train/test dataset ###
 
 # get full lists of img
@@ -63,7 +62,7 @@ y_test <- as.array(cnn_input$y)[-ind_train]
 
 ###  keras algorithm
 
-model <- keras_model_sequential() %>% 
+cnn_model <- keras_model_sequential() %>% 
   layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = "relu", 
                 input_shape = c(height_size,width_size,channel)) %>% 
   layer_max_pooling_2d(pool_size = c(2,2)) %>% 
@@ -71,20 +70,19 @@ model <- keras_model_sequential() %>%
   layer_max_pooling_2d(pool_size = c(2,2)) %>% 
   layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = "relu")
 
-model %>% 
+cnn_model %>% 
   layer_flatten() %>% 
   layer_dense(units = 64, activation = "relu") %>% 
   layer_dense(units = 1, activation = "sigmoid")
 
 
-model %>% compile(
+cnn_model %>% compile(
   optimizer = "adam",
   loss = "binary_crossentropy",
   metrics = "accuracy"
 )
 
-
-history <- model %>% 
+history <- cnn_model %>% 
   fit(
     x = x_train, y = y_train,
     epochs = 4, batch_size = 4,
@@ -93,6 +91,71 @@ history <- model %>%
   )
 
 plot(history)
+
+
+################################################### 
+################# LIME interpretability ################
+################################################### 
+
+
+list_files
+img_path <- paste0(path_abs,method_name,"/",list_files)
+
+image_prep2 <- function(x) {
+  arrays <- lapply(x, function(path) {
+    img <- image_load(path, target_size = c(180, 320))
+    x <- image_to_array(img)
+    x <- array_reshape(x, c(1, dim(x)))
+    x <- imagenet_preprocess_input(x)
+  })
+  do.call(abind::abind, c(arrays, list(along = 1)))
+}
+
+
+# code chunk to manage superpixels combination 
+pdf("data/test_pixels/test2020.pdf")
+plots <- map(img_path[1:6],plot_superpixels,n_superpixels = 30, weight = 20) 
+dev.off()
+# possibility for superpixels combination
+# pref : (30,30) ; (30,40) ;  best with 30, if too long (20,30) ; (20,40)
+
+
+# write a function that is creating the necessary LIME outputs for all the 16*34 heatmaps
+
+
+
+lime_explanator <- function(model = cnn_model){
+  
+  
+  
+}
+
+explainer2 <- lime(c(img_path, img_path2), model = model, 
+                   preprocess =  image_prep2)
+
+explanation2 <- explain(c(img_path, img_path2), explainer2,
+                        n_labels = 2, n_features = 10, weight = 10,n_superpixels = 10,
+                        background = "white")
+
+exp <- as.data.frame(explanation2)
+desagreable <- exp[exp$case == "fake43.png",]
+plot_image_explanation(desagreable)
+agreable <- exp[exp$case == "fake100.png",]
+plot_image_explanation(agreable)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
